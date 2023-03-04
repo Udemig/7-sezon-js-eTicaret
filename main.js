d04ba0f8af632c1d89945b7f5512c5b04c1734f1
furkanevin <furkanevin00@gmail.com>
2023-03-05 01:18:41 +0300

    proje finish

--- main.js
+++ main.js
@@ -21,3 +21,58 @@ document.addEventListener('DOMContentLoaded', function () {
       });
     });
 });
+
+var modal = document.querySelector(".modal")
+var toplamBilgi = document.querySelector("#toplamBilgi")
+// sepete ekleme
+let fiyatlar = []
+
+function sepeteEkle(parametre){
+  var urunDiv = document.createElement("div")
+  urunDiv.classList.add("urunDiv")
+  urunDiv.innerHTML = `
+          <h1> ${parametre.name}</h1>
+          <h2>${parametre.price} TL</h2>
+  `;
+  modal.appendChild(urunDiv)
+
+  fiyatlar.push(Number(parametre.price))
+  const toplam = fiyatlar.reduce((a,b)=>a + b,0)
+  toplamBilgi.innerHTML = toplam
+}
+
+//ÜRÜNLERİ ÇEKME VE YAZMA
+var productList = document.getElementById("products")
+
+document.addEventListener("DOMContentLoaded",function(){
+   fetch('https://api.escuelajs.co/api/v1/products')
+   .then(function(response){return response.json()})
+   .then(function(data){ data.slice(0,20).forEach(function(product){
+    console.log(product)
+    //ÜRÜN OLUŞTURMA
+    var productItem = document.createElement("div")
+    productItem.classList.add("product")
+    productItem.innerHTML = `
+          <img src="${product.images[0]}" alt="">
+          <p>${product.title}</p>
+          <label>${product.category.name}</label>
+          <div class="card-buy">
+            <label> ${product.price} TL</label>
+            <button onclick="sepeteEkle({name:'${product.title}', price:'${product.price}' })" >Sepete Ekle</button>
+          </div>
+    `;
+    productList.appendChild(productItem)   }) })
+})
+
+// SEPET KISMI
+var sepetBtn = document.getElementById("sepet")
+var modalBox = document.querySelector(".modal-box")
+var closeBtn = document.getElementById('close');
+
+sepetBtn.addEventListener("click",function(){
+  modalBox.classList.add('active');
+})
+
+closeBtn.addEventListener("click",function(){
+  modalBox.classList.remove("active")
+})
